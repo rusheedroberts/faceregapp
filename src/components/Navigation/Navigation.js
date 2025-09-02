0f440c73d5bc0c368feb3efe8c85621fa67b7e7d
rusheedroberts <rusheed.roberts@gmail.com>
2025-09-02 15:34:54 -0400

    project updated

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Navigation =() =>{
+const Navigation = () => {
 
 	return (
-		<nav>
-			<p> Sign Out </p>
+		<nav style= {{display: 'flex', justifyContent: 'flex-end'}}>
+			<p className='f3 link dim black underline pa3 pointer'>Sign Out</p>
 		</nav>
-			);
+	);
 }
 
 export default Navigation;
