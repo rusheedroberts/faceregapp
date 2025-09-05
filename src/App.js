8b093e5ed60c33e8887a2d833bca2e75532f82c2
rusheedroberts <rusheed.roberts@gmail.com>
2025-09-05 15:36:06 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -1,21 +1,89 @@
 import React, { Component} from 'react';
 import Navigation from './components/Navigation/Navigation';
-import Logo from './components/Logo/Logo';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
-import Rank from './components/Rank/Rank';
-import './App.css';
 import ParticlesBg from 'particles-bg';
+import Logo from './components/Logo/Logo';
+import Rank from './components/Rank/Rank';
+import Clarifai from 'clarifai';
+import './App.css';
 
+
+const app = new Clarifai.App({
+  apiKey: '6ef5676d3f414b2094af6d7791bfc9e3'
+});
+
+
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value :50,
+      density: {
+        enable:true,
+        value_area:800
+      }
+    }
+  }
+}
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input:'',
+      imageUrl: '',
+       box: {},
+    }
+  }
+
+   calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+   }
+
+    displayFaceBox = (box) => {
+    this.setState({box: box});
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    this.setState ({imageUrl: this.state.input});
+    app.models
+      .predict(
+        Clarifai.FACE_DETECT_MODEL,
+        this.state.input)
+        .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+          .catch(err => console.log(err));
+      
+      }
+
 render () {
   return (
     <div className="App">
+      <ParticlesBg 
+        className='particles' 
+        params ={particlesOptions} 
+        type="cobweb" 
+        bg={true} />
       <Navigation />     
       <Logo />
-      <ImageLinkForm />
       <Rank />
-          {/*  <FaceRecognition />*/}
+      <ImageLinkForm 
+        onInputChange={this.onInputChange} 
+        onButtonSubmit={this.onButtonSubmit}  />      
+      <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
 
      
     </div>
